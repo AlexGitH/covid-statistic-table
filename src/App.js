e90304981a278a9e2514da4ff27539578ef9695d
AlexP <alexander.pristavka@gmail.com>
2021-07-03 09:26:16 +0300

    Create generator function for testing data inflation;

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import './App.css';
 import CovidTable from './components/CovidTable';
 
+function* getColumnGenerator( current=0, step=1 ) {
+  while( true ){
+    yield {
+      dataIndex:`header${current}`,
+      title : `HEADER_${current}`
+    };
+    current += step;
+  }
+}
+
+
+const getColumnConfig = getColumnGenerator( 0 );
+
+const columns = Array.from({
+  length : 4
+}, ()=>getColumnConfig.next().value )
+
 function App() {
+
   return (
     <div className="App">
-      <CovidTable />
+      <CovidTable columns={columns}/>
     </div>
   );
 }
