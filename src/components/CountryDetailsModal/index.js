f3efc2ce273a7be20da71e35ac3f33bea9bb056a
AlexP <alexander.pristavka@gmail.com>
2021-07-05 00:36:45 +0300

    Create basic modal window component;

---
+++ src/components/CountryDetailsModal/index.js
@@ -0,0 +1,22 @@
+import './style.css';
+
+const CountryDetailsModal = ()=>{
+  return (
+    <div className="CountryDetailsModal">
+      <div className="content">
+        <div className="header">
+          <h4>Albania</h4>
+        </div>
+        <div className="body">
+          <ul>
+            <li>Stat 1</li>
+            <li>Stat 2</li>
+            <li>Stat 3</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default CountryDetailsModal;
