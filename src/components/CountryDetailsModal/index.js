0edc22a62d64236e1e2083b15bbbdb697170c283
AlexP <alexander.pristavka@gmail.com>
2021-07-05 11:49:47 +0300

    Improve country details modal markup; Create button;

--- src/components/CountryDetailsModal/index.js
+++ src/components/CountryDetailsModal/index.js
@@ -1,18 +1,36 @@
 import './style.css';
+import confirmed from './confirmed.svg';
+import deaths from './deaths.svg';
+import recovered from './recovered.svg';
 
-const CountryDetailsModal = ()=>{
+// DEBUG: remove after testing
+const testData = [
+  [confirmed,'Total Confirmed', 132215],
+  [deaths,'Total Deaths', 2812],
+  [recovered,'Total Recovered', 13230]
+]
+
+const CountryDetailsModal = ({details=testData})=>{
   return (
     <div className="CountryDetailsModal">
-      <div className="content">
-        <div className="header">
+      <div className="country-modal-content">
+        <div className="country-modal-header">
           <h4>Albania</h4>
         </div>
-        <div className="body">
-          <ul>
-            <li>Stat 1</li>
-            <li>Stat 2</li>
-            <li>Stat 3</li>
-          </ul>
+        <div className="country-modal-body">
+          <table>
+            <tbody>
+              {details.map(([ico,prop,number])=><tr key={prop}>
+                <td>
+                  <img src={ico} alt={prop}/>
+                </td>
+                <td>{prop}</td>
+                <td>{number}</td></tr>)}
+            </tbody>
+          </table>
+        </div>
+        <div className="country-modal-footer">
+          <button>OK</button>
         </div>
       </div>
     </div>
