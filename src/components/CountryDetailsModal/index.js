2d204fdcf33ea0cd0998ed4206f4a5d2bf1f03b8
AlexP <alexander.pristavka@gmail.com>
2021-07-07 02:31:38 +0300

    Adopt CountryDetails component for incoming data;

--- src/components/CountryDetailsModal/index.js
+++ src/components/CountryDetailsModal/index.js
@@ -3,15 +3,16 @@ import confirmed from './confirmed.svg';
 import deaths from './deaths.svg';
 import recovered from './recovered.svg';
 
-// DEBUG: remove after testing
-const testData = [
-  [confirmed,'Total Confirmed', 132215],
-  [deaths,'Total Deaths', 2812],
-  [recovered,'Total Recovered', 13230]
-]
 
-const CountryDetailsModal = ({details=testData})=>{
+const CountryDetailsModal = ({ isVisible, data: { TotalConfirmed, TotalDeaths, TotalRecovered } , onOk })=>{
+  const details = [
+    [confirmed,'Total Confirmed', TotalConfirmed],
+    [deaths,'Total Deaths', TotalDeaths],
+    [recovered,'Total Recovered', TotalRecovered]
+  ]
   return (
+    <>
+    {isVisible &&
     <div className="CountryDetailsModal">
       <div className="country-modal-content">
         <div className="country-modal-header">
@@ -30,10 +31,12 @@ const CountryDetailsModal = ({details=testData})=>{
           </table>
         </div>
         <div className="country-modal-footer">
-          <button>OK</button>
+          <button onClick={onOk}>OK</button>
         </div>
       </div>
     </div>
+    }
+    </>
   );
 }
 
