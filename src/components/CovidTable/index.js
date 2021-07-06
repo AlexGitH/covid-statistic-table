f7cbc740ec972039a9fe3e9b466f4f1ba0c2c27a
AlexP <alexander.pristavka@gmail.com>
2021-07-07 01:29:27 +0300

    Improve CovidTable for using SortDirection component;

--- src/components/CovidTable/index.js
+++ src/components/CovidTable/index.js
@@ -1,10 +1,12 @@
 import './style.css';
 import loading from './loading-tr.gif'
+import SortDirection from './SortDirection';
 
 const Preloader = ()=><img className="loader" src={loading} alt="Loading..." />
 const Error = ( {text} )=><h1 className="error">{text}</h1>
 
-const CovidTable = ({countries, visibleCountries, error, sortingField, columns, sortField})=>{
+const CovidTable = ({countries, visibleCountries, error, sortingField, columns, onSort})=>{
+  const { dataIndex:sortingDataIndex, isDesc } = sortingField;
   return (
     error != null
       ? <Error text={error} />
@@ -12,7 +14,24 @@ const CovidTable = ({countries, visibleCountries, error, sortingField, columns, 
         ? <>
             <table className="CovidTable">
                 <thead>
-                    <tr>{ columns.map( ({dataIndex,title}) => ( <th key={dataIndex} onClick={ () => sortField(countries,dataIndex,sortingField ) }>{title}</th>))}</tr>
+                    {/* <tr>{ columns.map( ({dataIndex,title}) => ( <th key={dataIndex} onClick={ () => onSort(countries,dataIndex,sortingField ) }>{title}</th>))}</tr> */}
+                    <tr>{ columns.map( ({dataIndex,title}) => (
+                      <th key={dataIndex} onClick={ () => onSort(countries,dataIndex,sortingField ) }>
+                        <div>
+                          <div>
+                            <div>
+                              {title}
+                            </div>
+                          </div>
+                          <SortDirection
+                            className="sort-direction"
+                            dataIndex={dataIndex}
+                            sortingDataIndex={sortingDataIndex}
+                            isDesc={isDesc}
+                          />
+                        </div>
+                        </th>))}
+                    </tr>
                 </thead>
 
                 <tbody>
