e9ca5ebdde841446997a408a535325eb3a20923c
AlexP <alexander.pristavka@gmail.com>
2021-07-07 12:27:16 +0300

    Cleanup SortDirection;

--- src/components/CovidTable/SortDirection.js
+++ src/components/CovidTable/SortDirection.js
@@ -1,10 +1,6 @@
 import './SortDirection.css';
 
 const SortDirection = ({dataIndex, sortingDataIndex, isDesc}) => 
-    //  ( dataIndex === sortingDataIndex) && <div className='sort-direction'>
-    //     {isDesc ? 'v' : '^'}
-    //  </div> 
-
     <div className='sort-direction'>
      {( dataIndex === sortingDataIndex)
         ? isDesc ? '↓' : '↑'
