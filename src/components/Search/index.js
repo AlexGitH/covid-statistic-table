4d11d82dd4d44077fe0b4e4a7f7f5b5bf4985a0c
AlexP <alexander.pristavka@gmail.com>
2021-07-07 09:39:49 +0300

    Remove hooks from Search and use connected props instead;

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,22 +1,15 @@
-import React, { useState, useEffect } from 'react';
 import './style.css';
 import scope from './scope.svg';
 
 
-const Search = ( {countries, filterCovidTable} ) => {
-  const [search, setSearch] = useState('');
-  useEffect(() => {
-    filterCovidTable( countries, search );
-  }, [search] )
-
-
+const Search = ( { search, countries, filterCovidTable} ) => {
 
   return <div className="Search" >
     <input
     type="text"
     placeholder="Search..."
     value={search}
-    onChange={ e => setSearch( e.target.value ) } />
+    onChange={ e => filterCovidTable( countries, e.target.value ) } />
     
     <img src={scope} alt="Search" />
   </div>
